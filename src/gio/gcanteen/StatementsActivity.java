8407498bd6e837cef35b3b2edad7611228debe1d
Giovanni Mascellani <giovanni@mascellani.eu>
2020-10-20 10:14:00 +0200

    ???

--- src/gio/gcanteen/StatementsActivity.java
+++ src/gio/gcanteen/StatementsActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 public class StatementsActivity extends Activity {
 
